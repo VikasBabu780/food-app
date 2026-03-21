020fcc61b0100b5b747e96ba18f62f0866eaf001
Vikas Babu <23bma048@nith.ac.in>
2026-03-21 23:52:50 +0530

    frontend url edited

--- server/index.ts
+++ server/index.ts
@@ -34,7 +34,7 @@ const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
       ? process.env.CLIENT_URL
-      : "food-702jd868r-vikas-babus-projects.vercel.app",
+      : "food-app-psi-nine.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
