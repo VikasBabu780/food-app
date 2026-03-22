c9dd66b5ac8344cefe4b5d3aef0c00a3908df843
Vikas Babu <23bma048@nith.ac.in>
2026-03-22 22:11:03 +0530

    mail error fixed

--- server/mailer/mail.config.ts
+++ server/mailer/mail.config.ts
@@ -6,26 +6,18 @@ export const getTransporter = async () => {
   if (_transporter) return _transporter;
 
   if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-    throw new Error(" EMAIL_USER or EMAIL_PASS is missing in .env");
+    throw new Error("EMAIL_USER or EMAIL_PASS is missing");
   }
 
   _transporter = nodemailer.createTransport({
-  service: "gmail",   //  simpler & safer
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  debug: true,
-  logger: true,
-});
-  // ERIFY CONNECTION (VERY IMPORTANT)
-  try {
-    await _transporter.verify();
-    // console.log(" Email server is ready");
-  } catch (err) {
-    console.error(" Transporter verification failed:", err);
-    throw err;
-  }
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 10000,
+  });
 
+  //  REMOVE verify()
   return _transporter;
 };
